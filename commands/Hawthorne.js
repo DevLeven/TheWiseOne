b70d3f91db9d84c1451acd36137cd56fbb3c9c57
DaRealLevenGamer <43507866+DaRealLevenGamer@users.noreply.github.com>
2020-09-19 21:01:36 -0400

    First

--- commands/Hawthorne.js
+++ commands/Hawthorne.js
@@ -1,5 +1,5 @@
 module.exports = {
-    name: 'DivineTales',
+    name: 'divinetales',
     description: 'Call command',
     execute(message, args) {
         message.channel.send('This is kingdom Hawthorne the dragon race!');
