e77233f8827a95ccb9280eac3948fdaaaf651d9f
DaRealLevenGamer <43507866+DaRealLevenGamer@users.noreply.github.com>
2020-09-21 11:06:39 -0400

    first

--- index.js
+++ index.js
@@ -73,6 +73,35 @@ bot.on('guildBanAdd', (guild, user) => {
         // not called
     });
 });
+
+bot.on('guildBanRemove', (guild, user) => {
+    const logChannel = guild.channels.find(ch => ch.name.includes('log'));
+    const unbanText = `${user.tag} has been welcomed back into the kingdom!`;
+
+    if (!logChannel) {
+        console.log('Could not find the report tab. Creating one now...');
+        guild.createChannel('logs', {
+                type: 'text',
+                position: 0,
+                topic: 'Report tab for the server to keep things in track!',
+                permissionOverwrites: [{
+                    id: guild.id,
+                    deny: ['SEND_MESSAGES'],
+                    allow: ['READ_MESSAGE_HISTORY', 'VIEW_CHANNEL']
+                }]
+            })
+            .then(console.log)
+            .catch(console.error);
+    }
+
+    Promise.resolve(unbanText).then(function (unbanText) {
+        logChannel.send(unbanText); // "Success"
+    }, function (value) {
+        // not called
+    });
+});
+
+
 bot.on("message", message => {
     if (!message.content.startsWith(prefix) || message.author.bot) return;
 
