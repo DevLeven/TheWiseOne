2249c6d1674f1497c6d704c987dc4f4f14234586
DaRealLevenGamer <43507866+DaRealLevenGamer@users.noreply.github.com>
2020-09-21 10:27:28 -0400

    first

--- index.js
+++ index.js
@@ -47,30 +47,32 @@ bot.on('guildMemberAdd', member => {
     })
 })
 
-bot.on('guildBanAdd', (user, guild) => {
+bot.on('guildBanAdd', (guild, user) => {
+    const logChannel = guild.channels.find(ch => ch.name.includes('log'));
     const banText = `${user.tag} has been exsiled from the kingdom!`;
-    const logChannel = user.guild.channels.find(ch => ch.name.includes('log'));
 
     if(!logchannel) {
         console.log('Could not find the report tab. Creating one now...');
-
         guild.createChannel('logs', {
-            type: 'text',
-            position: 0,
-            topic: 'Report tab for the server to keep things in track!',
-            permissionOverwrites: [{
-                id: guild.id,
-                allow: ['READ_MESSAGE_HISTORY', 'READ_MESSAGES'],
-                deny: ['SEND_MESSAGES']
-            }]
-        }).then(console.log('The report tab has officially been craeted!')).catch(console.error);
+                type: 'text',
+                position: 0,
+                topic: 'Report tab for the server to keep things in track!',
+                permissionOverwrites: [{
+                    id: guild.id,
+                    deny: ['SEND_MESSAGES'],
+                    allow: ['READ_MESSAGE_HISTORY', 'VIEW_CHANNEL']
+                }]
+            })
+            .then(console.log)
+            .catch(console.error);
     }
 
     Promise.resolve(banText).then(function (banText) {
-        logChannel.send(banText);
-    })
+        logChannel.send(banText); // "Success"
+    }, function (value) {
+        // not called
+    });
 });
-
 bot.on("message", message => {
     if (!message.content.startsWith(prefix) || message.author.bot) return;
 
