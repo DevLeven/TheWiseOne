1b50f24eee4d2e32b553430f44b7516c3b595346
DaRealLevenGamer <43507866+DaRealLevenGamer@users.noreply.github.com>
2020-10-01 10:23:10 -0400

    first

---
+++ commands/user-info.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js')
+
+module.exports = {
+    name: 'user-info',
+    description: 'Shows user info when user is mentoned!',
+    execute(message, args) {
+        let mentionedUser = message.mentions.members.first();
+        let mentionedUser = message.mentions.users.first();
+
+        const userEmbed = new Discord.RichEmbed()
+        .setTitle(`User information for ${mentionedUser.username}`)
+        .addField('Username:', `${mentionedUser.username}`)
+        .addField('User ID:', `${mentionedUser.id}`)
+        .addField('Account created:', `${mentionedUser.createdAt}`)
+        .addField('Joined the server at:', `${mentionedMember.joinedAt}`)
+        .addField('User status:', `${mentionedUser.presence.status}`)
+
+        message.channel.send(userEmbed).catch(err => console.log(err));
+    }
+}
