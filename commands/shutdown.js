141d6b6414dbcb7dbcc80e2c660e89806ea31fbc
DaRealLevenGamer <43507866+DaRealLevenGamer@users.noreply.github.com>
2020-10-02 18:36:54 -0400

    first
    Hopefully fixes bugs by reloading and shuting down the bot only the creator Leven can use the commands.

---
+++ commands/shutdown.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js')
+const botconfig = require('../botconfig.json');
+
+module.exports.config = {
+    name: 'shutdown',
+    description: 'stops the bot',
+    aliases: [],
+    execute(message, args) {
+    }
+}
+
+module.exports.run = async (bot, message, args,) => {
+
+    if(message.author.id != '345238773719760899') return message.channel.send('You didnt create me so you dont have the power to control me!')
+
+    try {
+        await message.channel.send('ThwWiseOne is asleep wake him up in your time of need!')
+        process.exit()
+    } catch(e) {
+        message.channel.send(`ERROR: ${e.message}`)
+    }
+
+
+}
