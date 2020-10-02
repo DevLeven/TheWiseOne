42e976481b7cdf18d0fe2b423eb554dbe43b5f52
DaRealLevenGamer <43507866+DaRealLevenGamer@users.noreply.github.com>
2020-10-02 19:02:21 -0400

    first
    fixed the async problem

--- commands/shutdown.js
+++ commands/shutdown.js
@@ -4,7 +4,7 @@ const botconfig = require('../botconfig.json');
 module.exports.config = {
     name: 'shutdown',
     description: 'stops the bot',
-    execute(message, args) {
+    async execute(message, args) {
         if (message.author.id != '345238773719760899') return message.channel.send('You didnt create me so you dont have the power to control me!')
 
         try {
