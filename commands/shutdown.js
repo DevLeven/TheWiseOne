3b5a305854175e28aa54174d2f5e42c80a77971f
DaRealLevenGamer <43507866+DaRealLevenGamer@users.noreply.github.com>
2020-10-02 18:59:47 -0400

    first
    Second fix to the excecute problem

--- commands/shutdown.js
+++ commands/shutdown.js
@@ -5,18 +5,15 @@ module.exports.config = {
     name: 'shutdown',
     description: 'stops the bot',
     execute(message, args) {
-        module.exports.run = async (bot, message, args,) => {
+        if (message.author.id != '345238773719760899') return message.channel.send('You didnt create me so you dont have the power to control me!')
 
-            if(message.author.id != '345238773719760899') return message.channel.send('You didnt create me so you dont have the power to control me!')
-        
-            try {
-                await message.channel.send('ThwWiseOne is asleep wake him up in your time of need!')
-                process.exit()
-            } catch(e) {
-                message.channel.send(`ERROR: ${e.message}`)
-            }
-        
-        
+        try {
+            await message.channel.send('ThwWiseOne is asleep wake him up in your time of need!')
+            process.exit()
+        } catch (e) {
+            message.channel.send(`ERROR: ${e.message}`)
         }
+
+
     }
 }
