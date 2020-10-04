5d4a2bf56bb62b7e016f32847734de0d25f99244
DaRealLevenGamer <43507866+DaRealLevenGamer@users.noreply.github.com>
2020-10-03 21:13:26 -0400

    first
    removed useless doubles

--- index.js
+++ index.js
@@ -44,11 +44,6 @@ bot.on('message', async message => {
 })
 
 
-if (!message.content.startsWith(prefix)) return;
-let commandfile = bot.commands.get(cmd.slice(prefix.length)) || bot.commands.get(bot.aliases.get(cmd.slice(prefix.length)))
-if (commandfile) commandfile.run(bot, message, args)
-
-
 const commandFiles = fs.readdirSync('./commands').filter(file => file.
     endsWith('.js'));
 
