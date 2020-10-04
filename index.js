f7bcdb24c1275c5c02fa2c516c2074f8a130e8cb
DaRealLevenGamer <43507866+DaRealLevenGamer@users.noreply.github.com>
2020-10-04 10:20:33 -0400

    first
    hopefully I made a fix I just reverted it to the original state.

--- index.js
+++ index.js
@@ -5,52 +5,6 @@ const fs = require('fs');
 const bot = new Discord.Client({disableEveryone: true});
 
 bot.commands = new Discord.Collection();
-bot.aliases = new Discord.Collection();
-
-fs.readdir('./commands/', (err, files) => {
-
-    if(err) console.log(err)
-
-    let jsfile = files.filter(f => f.split('.').pop() === 'js')
-    if(jsfile.length <= 0) {
-         return console.log('[LOGS] Couldnt find commands!');
-    }
-
-    jsfile.forEach((f, i,) => {
-        let pull = require(`./commands/${f}`);
-        bot.commands.set(pull.config.name, pull);
-        pull.config.aliases.forEach(alias => {
-            bot.aliases.set(alias, pull.config.name)
-        });
-    });
-});
-
-
-
-bot.on('message', async message => {
-    if(message.author.bot || message.channel.type === 'dm') return;
-
-    let prefix = botconfig.prefix;
-    let messageArray = message.content.split('')
-    let cmd = messageArray[0];
-    let args = messageArray.slice(1);
-
-
-    if(!message.content.startsWith(prefix)) return;
-    let commandfile = bot.commands.get(cmd.slice(prefix.length)) || bot.commands.get(bot.aliases.get(cmd.slice(prefix.length)))
-    if(commandfile) commandfile.run(bot, message, args)
-
-})
-
-
-const commandFiles = fs.readdirSync('./commands').filter(file => file.
-    endsWith('.js'));
-
-for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
-
-    bot.commands.set(command.name, command);
-}
 
 bot.on('ready', () => {
     console.log('The wise one has awoken.');
