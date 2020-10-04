bca4272d851aad671822c8d05b2e32a1ae561190
DaRealLevenGamer <43507866+DaRealLevenGamer@users.noreply.github.com>
2020-10-03 21:07:54 -0400

    first
    Hopefully fixed the problem with the not define should be fixed.

--- index.js
+++ index.js
@@ -26,6 +26,24 @@ fs.readdir('./commands/', (err, files) => {
     });
 });
 
+
+
+bot.on('message', async message => {
+    if(message.author.bot || message.channel.type === 'dm') return;
+
+    let prefix = botconfig.prefix;
+    let messageArray = message.content.split('')
+    let cmd = messageArray[0];
+    let args = messageArray.slice(1);
+
+
+    if(!message.content.startsWith(prefix)) return;
+let commandfile = bot.commands.get(cmd.slice(prefix.length)) || bot.commands.get(bot.aliases.get(cmd.slice(prefix.length)))
+if(commandfile) commandfile.run(bot,message,args)
+
+})
+
+
 if(!message.content.startsWith(prefix)) return;
 let commandfile = bot.commands.get(cmd.slice(prefix.length)) || bot.commands.get(bot.aliases.get(cmd.slice(prefix.length)))
 if(commandfile) commandfile.run(bot,message,args)
