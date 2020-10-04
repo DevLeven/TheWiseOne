10ead6f8232e4fd8f283548769b883dfdd994341
DaRealLevenGamer <43507866+DaRealLevenGamer@users.noreply.github.com>
2020-10-03 20:55:33 -0400

    first
    Hopefully I didnt mess up all my work but I hopefully fixed alot of problems.

--- index.js
+++ index.js
@@ -6,6 +6,30 @@ const fs = require('fs');
 const bot = new Discord.Client({disableEveryone: true});
 
 bot.commands = new Discord.Collection();
+bot.aliases = new Discord.Collection();
+
+fs.readdir('./commands/', (err, files) => {
+
+    if(err) console.log(err)
+
+    let jsfile = files.filter(f => f.split('.').pop() === 'js')
+    if(jsfile.length <= 0) {
+        return console.log('[LOGS] Couldnt find commands!');
+    }
+
+    jsfile.forEach((f, i,) => {
+        let pull = require(`./commands/${f}`);
+        bot.commands.set(pull.config.name, pull);
+        pull.config.aliases.forEach(alias => {
+            bot.aliases.set(alias, pull.config.name)
+        });
+    });
+});
+
+if(!message.content.startsWith(prefix)) return;
+let commandfile = bot.commands.get(cmd.slice(prefix.length)) || bot.commands.get(bot.aliases.get(cmd.slice(prefix.length)))
+if(commandfile) commandfile.run(bot,message,args)
+
 
 const commandFiles = fs.readdirSync('./commands').filter(file => file.
 endsWith('.js'));
