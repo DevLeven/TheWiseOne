de437b04c014cbbb1acb598632ebd8dded07b26a
DaRealLevenGamer <43507866+DaRealLevenGamer@users.noreply.github.com>
2021-02-06 23:32:34 -0500

    Update Hawthorne.js

--- commands/Hawthorne.js
+++ commands/Hawthorne.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'divinetales',
     description: 'Call command',
     execute(message, args) {
-        message.channel.send('This is kingdom Hawthorne the dragon race!');
+        message.channel.send('Divine Tales is a Roleplay server with custom lore, spells, and a well-written story. It takes place in the Land of Valera which is inhabited with many races, Its vast land holds ancient secrets and mysteries, Will you discover and uncover the secrets this land holds? Or will you fall to dark magicThis is kingdom Hawthorne the dragon race!');
     }
 }
